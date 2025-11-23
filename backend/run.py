7b46c2b004f4b757f91fe86fda9efcc789384086
Rahul Tiwari <jprrahultiwari@gmail.com>
2025-11-23 00:18:32 -0500

    change port

--- backend/run.py
+++ backend/run.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=80,
         reload=True,  # Enable auto-reload in development
         log_level="info",
         ssl_keyfile="key.pem",
